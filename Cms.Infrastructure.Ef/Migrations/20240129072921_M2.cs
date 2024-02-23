170d79de89a92d2c1cbc06970ce51e7a1c1ac429
Iskender-crypto <iskenderazamov@gmail.com>
2024-02-23 11:42:43 +0600

    commit

---
+++ Cms.Infrastructure.Ef/Migrations/20240129072921_M2.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cms.Infrastructure.Ef.Migrations
+{
+    /// <inheritdoc />
+    public partial class M2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "FileId",
+                table: "OrderItem",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_FileId",
+                table: "OrderItem",
+                column: "FileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_File_FileId",
+                table: "OrderItem",
+                column: "FileId",
+                principalTable: "File",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_File_FileId",
+                table: "OrderItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_FileId",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "FileId",
+                table: "OrderItem");
+        }
+    }
+}
